d38dd8c5d7852b3ad4d78aaee967851240ae5aad
Ruben <68506854+pricken45@users.noreply.github.com>
2022-05-10 13:37:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,21 @@
 #include "engine/ConsoleEngine.h"
 
-const int WIDTH = 50;
+const int WIDTH = 51;
 const int HEIGHT = 12;
 const bool BORDER = true;
 
 int main() {
   ConsoleEngine engine(WIDTH, HEIGHT, BORDER);
-  int x = 5;
-  int y = 5;
-  int xSpeed = 2;
-  int ySpeed = 1;
+
   while (true) 
   {
-    if (x <= 1 || x+2 >= WIDTH-1) {
-      xSpeed *= -1;
-    }
-    if (y <= 0 || y+1 >= HEIGHT-1) {
-      ySpeed *= -1;
-    }
-    x+=xSpeed;
-    y+=ySpeed;
     engine.background(' ');
-    engine.setPoint(0, 0, '+');
-    engine.rect(x, y, 3, 2, 'X');
+
+    engine.fill('o');
+    engine.point(4, 4);
+    
     engine.render();
-    engine.clear(); 
-    engine.delay(300);  
+    engine.delay(300);
   }
 
   return 0;
